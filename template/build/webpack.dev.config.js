151c67eed77a695cc883141a737de0ec1a078148
Maks <maxim.pchelnikov@icandeliver.ru>
2018-02-08 17:15:23 +0300

    Fixed env variable in dev

--- template/build/webpack.dev.config.js
+++ template/build/webpack.dev.config.js
@@ -14,7 +14,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
+        NODE_ENV: '"development"'
       }
     }),
     new webpack.HotModuleReplacementPlugin(),
